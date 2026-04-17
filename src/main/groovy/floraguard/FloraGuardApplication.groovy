9a05f3bd28e5e0277e4c583d526f4732a1b2d5e8
Erick Ortiz <eo9977572@gmail.com>
2026-04-16 23:36:56 -0600

    Carga inicial codigo

---
+++ src/main/groovy/floraguard/FloraGuardApplication.groovy
@@ -0,0 +1,13 @@
+package floraguard
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class FloraGuardApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(FloraGuardApplication, args)
+    }
+
+}
---
+++ src/test/groovy/floraguard/FloraGuardApplicationTests.groovy
@@ -0,0 +1,13 @@
+package floraguard
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FloraGuardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
